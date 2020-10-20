fcb31fa2f97098219fc2f61a5192be3be59aa328
Cibele Sthefany <cibelesthefany@gmail.com>
2020-10-19 21:19:40 -0300

    ac3

--- primos.py
+++ primos.py
@@ -19,7 +19,7 @@ def nao_entre_em_panico():
         ehprimo = 1
         for i in range(2,numero):
             if numero % i == 0:
-                ehprimo
+                ehprimo = 0
                 break
         if (ehprimo):
             primos = primos + str(numero) + ','
